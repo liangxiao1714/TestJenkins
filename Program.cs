78d9937aa94ee53428f8042a682372ffc4ef1792
LIANGJUNLONG\Administrator <junlong.liang@gmail.com>
2018-10-29 16:57:38 +0800

    ...

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace testjenkins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
