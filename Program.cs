456ea2b8b46dbfc92b367cce4eeaaa71bbe461cf
LIANGJUNLONG\Administrator <junlong.liang@gmail.com>
2018-10-29 16:53:07 +0800

    测试

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testjenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
