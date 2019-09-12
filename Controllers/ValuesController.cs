db01343b56c8f19f6481db8a4d3f70e55aa121cb
梁君龙 <junlong.liang@gmail.com>
2019-09-12 16:38:16 +0800

    ### api/values

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace TestJenkins.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value10", "value20" };
         }
 
         // GET api/values/5
